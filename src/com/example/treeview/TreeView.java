01234a33c206f80f3c901e7e6d9224e5cc809b17
JB Gill <jbgill@familysearch.org>
2013-03-23 11:51:37 -0600

    added 3rd generation to tree, fixed some issues with scaling and centering tree

--- src/com/example/treeview/TreeView.java
+++ src/com/example/treeview/TreeView.java
@@ -12,16 +12,29 @@ import android.view.MotionEvent;
 
 public class TreeView extends ZoomView {
 
-  private Paint paint;
+  private Paint bracketLinePaint;
+  private Path bracketLinePath = new Path();
   
   private TreeNode rootNode = null;
   private TreeNode selectedNode = null;
   
-  // margin between left size of view and root node
-  private static final int LEFT_MARGIN = 5;
+  // margin between sides of view and nodes
+  private static final int MARGIN = 5;
   
   // horizontal gap between nodes
   private static final int H_GAP = 30;
+  
+  // minimum vertical gap between nodes
+  private static final int MIN_V_GAP = 30;
+  
+  // how much room will the tree take unscaled?
+  private int treeWidth=0;
+  private int treeHeight=0;
+  
+  // used in calculating initial scale and translate, and detecting size changes:
+  private int calcWidth=0;
+  private int calcHeight=0;
+  
   
   public TreeView(Context context) {
     super(context);
@@ -39,13 +52,30 @@ public class TreeView extends ZoomView {
   }
 
   private void init() {
-    
     // paint object for lines
-    paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-    paint.setColor(Color.BLUE);
-    paint.setStrokeWidth(2f);
-    paint.setStyle(Style.STROKE);
-
+    bracketLinePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    bracketLinePaint.setColor(Color.BLUE);
+    bracketLinePaint.setStrokeWidth(2f);
+    bracketLinePaint.setStyle(Style.STROKE);
+    
+    // how much unscaled room the tree takes:
+    treeWidth = TreeNode.WIDTH*3 + H_GAP*2 + MARGIN*2;
+    treeHeight = TreeNode.HEIGHT*4 + MIN_V_GAP*3 + MARGIN*2;
+  }
+  
+  private void calculateInitialScaleAndTranslate(Canvas canvas) {
+    CanvasCamera cc = getCanvasCamera();
+    int oldCalcWidth = calcWidth;
+    int oldCalcHeight = calcHeight;
+    calcWidth = this.getWidth();
+    calcHeight = this.getHeight();
+    // if our dimensions changed, we need to recenter the tree in the view:
+    if (oldCalcHeight!=calcHeight || oldCalcWidth!=calcWidth) {
+      float startScale = ((float)calcWidth) / ((float)treeWidth);
+      cc.setScale(startScale);
+      float heightDiff = calcHeight - (treeHeight*startScale);
+      cc.setTranslation(0, heightDiff/2/startScale);
+    }
   }
   
   public TreeNode getSelectedNode() {
@@ -67,7 +97,7 @@ public class TreeView extends ZoomView {
   @Override
   public void onClickEvent(MotionEvent event) {
     PointF transformedPoint = getAsAbsoluteCoordinate(event.getX(), event.getY());
-    TreeNode clickedNode = this.findNodeByContainingPoint(rootNode, transformedPoint);
+    TreeNode clickedNode = this.findNodeByContainedPoint(rootNode, transformedPoint);
     if (clickedNode != null) {
       if (selectedNode != null) {
         selectedNode.setSelected(false);
@@ -78,20 +108,20 @@ public class TreeView extends ZoomView {
     } 
   }
   
-  private TreeNode findNodeByContainingPoint(TreeNode node, PointF p) {
+  private TreeNode findNodeByContainedPoint(TreeNode node, PointF p) {
     if (node.isPointInBounds(p)) {
       return node;
     } else {
       TreeNode father = node.getFather();
       TreeNode mother = node.getMother();
       if (father != null) {
-        TreeNode n = findNodeByContainingPoint(father, p);
+        TreeNode n = findNodeByContainedPoint(father, p);
         if (n != null) {
           return n;
         }
       }
       if (mother != null) {
-        TreeNode n = findNodeByContainingPoint(mother, p);
+        TreeNode n = findNodeByContainedPoint(mother, p);
         if (n != null) {
           return n;
         }        
@@ -103,6 +133,9 @@ public class TreeView extends ZoomView {
   @Override
   public void onDraw(Canvas canvas) {
     super.onDraw(canvas);
+    // this call needs to be made every time because the 
+    //height fluctuates as the view settles
+    this.calculateInitialScaleAndTranslate(canvas);
     CanvasCamera cc = getCanvasCamera();
     float scaleFactor = cc.getScale();
     float translateX = cc.getTranslation().x;
@@ -115,44 +148,104 @@ public class TreeView extends ZoomView {
 
     /* put the canvas drawing code here: */
 
-    doCanvasPaint(canvas);
+    drawTree(canvas);
 
     /* end of drawing code */
 
     canvas.restore();
+    
   }
   
-  public void doCanvasPaint(Canvas canvas) {
-    int myHeight = this.getHeight();
-    int vCenter = (int) (myHeight / 2);
-    int vThird = (int) (myHeight / 3);
+  private void drawTree(Canvas canvas) {
+    //int myHeight = this.getHeight();
+    int vCenter = (int) (treeHeight / 2);
     int nodeWidth = TreeNode.WIDTH;
     int nodeHeight = TreeNode.HEIGHT;
     
-    float startX = LEFT_MARGIN + nodeWidth;
-    float startY = vCenter;
-    float endX = startX + (H_GAP/2);
-    float endY = startY;
-    canvas.drawLine(startX, startY, endX, endY, paint);
-    Path path = new Path();
-    startX = LEFT_MARGIN + nodeWidth + H_GAP;
-    startY = vThird;
-    path.moveTo(startX,  startY);
-    endX = LEFT_MARGIN + nodeWidth + H_GAP/2;
-    endY = vThird;
-    path.lineTo(endX,  endY);
-    endY = vThird*2;
-    path.lineTo(endX,  endY);
-    endX = LEFT_MARGIN + nodeWidth + H_GAP;
-    path.lineTo(endX, endY);
-    canvas.drawPath(path, paint);
-    
-    rootNode.draw(canvas, LEFT_MARGIN, vCenter - (nodeHeight/2));
+    rootNode.draw(canvas, MARGIN, vCenter - nodeHeight/2);
     TreeNode father = rootNode.getFather();
     TreeNode mother = rootNode.getMother();
-    father.draw(canvas, LEFT_MARGIN + nodeWidth + H_GAP, vThird - (nodeHeight/2));
-    mother.draw(canvas, LEFT_MARGIN + nodeWidth + H_GAP, (vThird*2) - (nodeHeight/2));
-    
+    if (father != null || mother != null) {
+      drawBracketLines(canvas, 
+                       MARGIN + nodeWidth, 
+                       MARGIN + nodeHeight + MIN_V_GAP/2, 
+                       H_GAP, 
+                       nodeHeight*2 + MIN_V_GAP*2);      
+    }
+    if (father != null) {
+      father.draw(canvas, 
+                  MARGIN + nodeWidth + H_GAP, 
+                  MARGIN + nodeHeight + MIN_V_GAP/2 - nodeHeight/2);
+      TreeNode fathersFather = father.getFather();
+      TreeNode fathersMother = father.getMother();
+      if (fathersFather != null || fathersMother != null) {
+        drawBracketLines(canvas, 
+            MARGIN + nodeWidth*2 + H_GAP, 
+            MARGIN + nodeHeight/2, 
+            H_GAP, 
+            nodeHeight + MIN_V_GAP);
+        if (fathersFather!=null) {
+          fathersFather.draw(canvas, 
+                             MARGIN + 2*H_GAP + 2*nodeWidth, 
+                             MARGIN);
+        }
+        if (fathersMother != null) {
+          fathersMother.draw(canvas, 
+                             MARGIN + 2*H_GAP + 2*nodeWidth, 
+                             MARGIN + MIN_V_GAP + nodeHeight);
+        }
+      }
+    }
+    if (mother != null) {
+      mother.draw(canvas, 
+                  MARGIN + nodeWidth + H_GAP, 
+                  MARGIN + nodeHeight*3 + MIN_V_GAP*2.5f - nodeHeight/2);
+      TreeNode mothersFather = mother.getFather();
+      TreeNode mothersMother = mother.getMother();
+      if (mothersFather != null || mothersMother != null) {
+        drawBracketLines(canvas, 
+            MARGIN + nodeWidth*2 + H_GAP, 
+            MARGIN + nodeHeight/2 + nodeHeight*2 + 2*MIN_V_GAP, 
+            H_GAP, 
+            nodeHeight + MIN_V_GAP);
+        if (mothersFather!=null) {
+          mothersFather.draw(canvas, 
+                             MARGIN + 2*H_GAP + 2*nodeWidth, 
+                             MARGIN + 2*MIN_V_GAP + 2*nodeHeight);
+        }
+        if (mothersMother != null) {
+          mothersMother.draw(canvas, 
+                             MARGIN + 2*H_GAP + 2*nodeWidth, 
+                             MARGIN + 3*MIN_V_GAP + 3*nodeHeight);
+        }
+      }
+    }
+
   }
+  
+  /**
+   * draw the lines that connect a node to its parent nodes
+   * @param canvas - the canvas to draw on
+   * @param x - the upper left corner x-coord of the box bounding the lines
+   * @param y - the upper left corner y-coord of the box bounding the lines
+   * @param width - the width of the box bounding the lines
+   * @param height - the height of the box bounding the lines
+   */
+  private void drawBracketLines(Canvas canvas, float x, float y, float width, float height) {
+    float middleY = y + (height/2);
+    float middleX = x + (width/2);
+    float rightX = x+width;
+    float bottomY = y+height;
+    canvas.drawLine(x, middleY, middleX, middleY, bracketLinePaint);
+    bracketLinePath.reset();
+    bracketLinePath.moveTo(rightX, y);
+    bracketLinePath.lineTo(middleX, y);
+    bracketLinePath.lineTo(middleX, bottomY);
+    bracketLinePath.lineTo(rightX, bottomY);
+    canvas.drawPath(bracketLinePath, bracketLinePaint);  
+  }
+  
+  
+  
 
 }
